3df5698320f8279daecebe8a74da32a6fb6dcda8
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-06-12 17:46:19 +0700

    mengisi void remove part 1

--- circularqueues.cpp
+++ circularqueues.cpp
@@ -49,5 +49,12 @@ public:
     }
 
     void remove()
-    
+    {
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+    }
 }
