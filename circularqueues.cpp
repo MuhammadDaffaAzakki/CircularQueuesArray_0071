ed7a123490ad57fffdf583210943baaff6f1b356
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-06-12 18:36:06 +0700

    mengisi void display part 4

--- circularqueues.cpp
+++ circularqueues.cpp
@@ -107,8 +107,14 @@ public:
                     FRONT_position++;
                 }
 
-                
+                // iterasi dari awal array hingga REAR
+                while (FRONT_position <= REAR_position)
+                {
+                    cout << queue_array[FRONT_position] << " ";
+                    FRONT_position++;
+                }
+                cout << endl;
             }
         }
-    }
+    };
 }
