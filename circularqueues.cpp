fcf639f4319135b75c24f8eb724bc31069292ba7
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-06-12 17:32:32 +0700

    membuat class queues dan membuat private

---
+++ circularqueues.cpp
@@ -0,0 +1,12 @@
+// CircularQueuesArray_0071
+// 15 commit
+#include <iostream>
+using namespace std;
+
+class Queues
+{
+private: 
+    static const int max = 5;
+    int FRONT, REAR;
+    int queue_array[5];
+}
